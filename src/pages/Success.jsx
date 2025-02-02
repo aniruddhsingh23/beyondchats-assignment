5f46790fac33fc5780401efb5c7d15da6a3a5d01
Aniruddh <anirudhbhadauriya@gmail.com>
2025-02-02 09:27:06 +0530

    Removed all files except frontend

--- src/pages/Success.jsx
+++
@@ -1,9 +0,0 @@
-import SuccessScreen from '../components/ChatIntegration/SuccessScreen'
-
-export default function Success() {
-  return (
-    <div className="success-screen">
-      <SuccessScreen />
-    </div>
-  )
-}
